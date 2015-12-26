c348bf3ec86c8155b9f00db46c6a44cc71d86198
Ryan Kulp <ryanckulp@gmail.com>
2015-12-26 16:14:15 -0500

    dont send SMS reminders on saturday or sunday

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -4,14 +4,18 @@ class Reminder < ActiveRecord::Base
 	phony_normalize :phone_number, default_country_code: 'US'
 
 	def self.send_reminders
-	  client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
+		today = Date.today.strftime('%a')
+		unless today == "Sat" || today == "Sun" # don't send reminders on weekend
 
-		users = get_users_with_reminders
-		users.each do |user|
-			client.messages.create(
-			    from: ENV['TWILIO_PHONE_NUMBER'],
-			    to: user,
-			    body: 'Drop down and give me some pushups, maggot! Then reply to this message with the amount you did for instant logging.')
+			client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
+
+			users = get_users_with_reminders
+			users.each do |user|
+				client.messages.create(
+				    from: ENV['TWILIO_PHONE_NUMBER'],
+				    to: user,
+				    body: 'Drop down and give me some pushups, maggot! Then reply to this message with the amount you did for instant logging.')
+			end
 		end
 	end
 
