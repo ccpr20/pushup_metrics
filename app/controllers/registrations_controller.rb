cc523364d5482a1e92d449c17e4a36136e338857
Ryan Kulp <ryanckulp@gmail.com>
2016-01-20 22:07:32 -0800

    allow nested attribute params

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,34 +1,52 @@
 class RegistrationsController < Devise::RegistrationsController
+  before_filter :configure_permitted_parameters
   after_action :set_team_slug, only: [:create]
 
-  def create
-    super
-    # custom notification not included in open-source version
-    Peanus.ping "new user! (#{@user.name} // #{@user.email})"
+  # GET /signup
+  def new
+    # Override Devise default behaviour and create a reminder as well
+    build_resource({})
+    @reminder = resource.reminders.build
+    # resource.build_reminder
+    respond_with self.resource
   end
 
-    private
-      def set_team_slug
-        # checks for existing team or creates new
-        @team = Team.find_by(subdomain: @subdomain)
-        if @team.present?
-          @user.teams << @team
-        else
-          team = Team.new(subdomain: @subdomain, name: @subdomain, user_id: @user.id)
-          team.save
-          @user.teams << team
-        end
-      end
+  # def create
+  #   super
+  #   Peanus.ping "new user! (#{@user.name} // #{@user.email})"
+  # end
+
+  protected
+
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.for(:sign_up) { |u|
+  #     u.permit(:email, :name, :company, :company_website, :password, reminders_attributes: [:phone_number, :user_id])
+  #   }
+  # end
+
+  private
+
+  def set_team_slug
+    # checks for existing team or creates new
+    @team = Team.find_by(subdomain: @subdomain)
+    if @team.present?
+      @user.teams << @team
+    else
+      team = Team.new(subdomain: @subdomain, name: @subdomain, user_id: @user.id)
+      team.save
+      @user.teams << team
+    end
+  end
 
 	def sign_up_params
-        params.require(:user).permit(:email, :name, :company, :company_website, :password)
+    params.require(:user).permit(:email, :name, :company, :company_website, :password, reminders_attributes: [:phone_number])
 	end
 
 	def account_update_params
-        params.require(:user).permit(:email, :name, :company, :company_website, :password)
+    params.require(:user).permit(:email, :name, :company, :company_website, :password)
 	end
 
-      def update_resource(resource, params)
-        resource.update_without_password(params)
-      end
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
 end
