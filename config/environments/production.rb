097a9b91bfdf3d7edc1fee75773c9a1efa82a8b6
Ryan Kulp <ryanckulp@gmail.com>
2016-01-20 16:17:46 -0800

    proper www host and restyle new password form

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.default_url_options = { :host => 'pushupmetrics.com' }
+  config.action_mailer.default_url_options = { :host => 'www.pushupmetrics.com' }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
