28e997b7a1e71cdf6fe3ba3bffcbebd5a2ca2d39
Ryan Kulp <ryanckulp@gmail.com>
2016-01-20 22:09:01 -0800

    accept nested attributes for reminders

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ActiveRecord::Base
 
 	has_many :pushups
 	has_many :reminders
+  accepts_nested_attributes_for :reminders
+    
 	has_and_belongs_to_many :teams
 	accepts_nested_attributes_for :teams
 
