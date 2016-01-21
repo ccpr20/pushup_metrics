f9ecc1f9aa4eca79eccb61a6130bd63ec4a3e7e2
Ryan Kulp <ryanckulp@gmail.com>
2016-01-20 16:08:59 -0800

    forgot password mailer

--- config/application.rb
+++ config/application.rb
@@ -9,5 +9,16 @@ Bundler.require(*Rails.groups)
 module Speedrail
   class Application < Rails::Application
     config.active_record.raise_in_transactional_callbacks = true
+
+    ActionMailer::Base.smtp_settings = {
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :domain => 'freelancedebt.com',
+      :address => 'smtp.sendgrid.net',
+      :port => 587,
+      :authentication => :plain,
+      :enable_starttls_auto => true
+    }
+
   end
 end
