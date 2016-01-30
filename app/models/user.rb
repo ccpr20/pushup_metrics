91d9aa312f189ae5cf13e9831aaeb84a07fd7bdf
Ryan Kulp <ryanckulp@gmail.com>
2016-01-30 00:17:09 -0800

    default to remember user login

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,14 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   validates_presence_of :email
 
+  def remember_me
+    true
+  end
+  
 	has_many :pushups
 	has_many :reminders
   accepts_nested_attributes_for :reminders
-    
+
 	has_and_belongs_to_many :teams
 	accepts_nested_attributes_for :teams
 
