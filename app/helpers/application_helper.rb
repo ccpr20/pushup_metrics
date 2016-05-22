1ac0f384e2fb9b5f05358f817245c7f3a47a4e10
Ryan Kulp <ryanckulp@gmail.com>
2016-05-22 00:55:48 -0700

    move custom reminder nav to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # header navigation
+  def reminder_link(user)
+    if user.reminders.count > 0
+      link_to "My Reminders", reminder_path(user.reminders.first.id)
+    else
+      link_to "Reminders", new_reminder_path
+    end
+  end
+
 end
