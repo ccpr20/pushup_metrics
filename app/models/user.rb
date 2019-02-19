fb44b9abf0390b418a38156029fad49220ff6b41
Ryan Kulp <ryanckulp@gmail.com>
2019-02-19 21:52:32 +0800

    better public name for leaderboards

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,18 @@ class User < ActiveRecord::Base
     pushups.inject(0) { |sum, p| sum += p.amount }
   end
 
+  def public_name
+    "#{first_name} #{last_name.try(:first).nil? ? '' : (last_name[0] + '.')}".strip
+  end
+
+  def first_name
+    name.split[0]
+  end
+
+  def last_name
+    name.split[-1]
+  end
+
   def most_recent_set
     pushups.order('date desc').first
   end
