a312f000c43aa6fd5f60406169931988e77337d5
vov <vov@vovs-iMac.local>
2020-01-20 14:28:14 +0200

    fix leader board data

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -1,26 +1,25 @@
 class LeaderboardController < ApplicationController
   def index
-    @user_ids = Pushup.where(created_at: 90.days.ago..Time.now).group('user_id').order('sum_amount').sum('amount')
-    @leaders = User.where(id: @user_ids.keys)#.order('id')
-    # sql = "
-    #   SELECT
-    #       a.*,
-    #       b.total_pushup 
-    #   FROM
-    #       users a
-    #   LEFT JOIN (
-    #       SELECT
-    #           user_id,
-    #           SUM(amount) total_pushup
-    #       FROM
-    #           pushups
-    #       WHERE
-    #           EXTRACT(DAY FROM now()-created_at) <= 90
-    #       GROUP BY user_id
-    #   ) b ON a.id = b.user_id
-    # "
-    # @leaders = ActiveRecord::Base.connection.execute(sql)
-    # logger.debug '***************************************************'
-    # logger.debug @leaders.inspect
+    # @user_ids = Pushup.where(created_at: 90.days.ago..Time.now).group('user_id').order('sum_amount').sum('amount')
+    # @leaders = User.where(id: @user_ids.keys)#.order('id')
+    sql = "
+      SELECT
+          a.*,
+          b.total_pushup 
+      FROM
+          users a
+      INNER JOIN (
+          SELECT
+              user_id,
+              SUM(amount) total_pushup
+          FROM
+              pushups
+          WHERE
+              EXTRACT(DAY FROM now()-created_at) <= 90
+          GROUP BY user_id
+      ) b ON a.id = b.user_id
+      ORDER BY total_pushup DESC
+    "
+    @leaders = ActiveRecord::Base.connection.execute(sql)
   end
 end
--- app/helpers/leaderboard_helper.rb
+++ app/helpers/leaderboard_helper.rb
@@ -1,5 +1,5 @@
 module LeaderboardHelper
   def public_avatar(user)
-    gravatar_tag user.email, size: 25, default: "https://api.adorable.io/avatars/25/#{user.email}"
+    gravatar_tag user['email'], size: 25, default: "https://api.adorable.io/avatars/25/#{user['email']}"
   end
 end
