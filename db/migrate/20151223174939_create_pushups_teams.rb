94123315b48ba9753f661f6a30d74b4911a04415
Talha Mehmood <talhacodes@gmail.com>
2023-03-21 19:04:40 +0500

    Fix migration file duplicating team_id

--- db/migrate/20151223174939_create_pushups_teams.rb
+++ db/migrate/20151223174939_create_pushups_teams.rb
@@ -5,6 +5,5 @@ class CreatePushupsTeams < ActiveRecord::Migration[5.1]
 			t.references :team
     end
 		add_index :pushups_teams, [:pushup_id, :team_id]
-		add_index :pushups_teams, :team_id
   end
 end
