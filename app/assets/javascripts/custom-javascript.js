abd188a1a0b9342e7230bb3218b7131b6c62e5f7
Talha Mehmood <talhacodes@gmail.com>
2023-04-18 13:26:33 +0500

    Fix JS issue on reload

--- app/assets/javascripts/custom-javascript.js
+++ app/assets/javascripts/custom-javascript.js
@@ -1,4 +1,4 @@
-$(document).on('turbolinks:load',function() {
+$(document).on('turbolinks:load', function() {
   var myEle = document.getElementById("dashboard-header");
   if(myEle) {
     $('.footer').removeClass('bottom');
