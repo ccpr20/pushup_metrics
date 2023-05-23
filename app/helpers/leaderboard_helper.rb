a487cdb6627ce840f47ec3426e71c412e7130ffb
Talha Mehmood <talhacodes@gmail.com>
2023-05-23 12:08:34 +0500

    Show age from dob.

--- app/helpers/leaderboard_helper.rb
+++ app/helpers/leaderboard_helper.rb
@@ -2,4 +2,16 @@ module LeaderboardHelper
   def public_avatar(user)
     gravatar_tag user['email'], size: 25, default: "https://api.adorable.io/avatars/25/#{user['email']}"
   end
+
+  def calculate_age(dob)
+    if dob != nil
+      current_date = Date.today
+      age = current_date.year - dob&.year
+
+      # Check if the birthday has already occurred this year
+      age -= 1 if current_date < dob + age.years
+
+      age
+    end
+  end
 end
