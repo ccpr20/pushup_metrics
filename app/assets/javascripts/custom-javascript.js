bcbdf3de04b93da2fff598b07779848cbf8e126d
Talha Mehmood <talhacodes@gmail.com>
2023-05-30 03:52:36 +0500

    Add leaderboard trailing.

--- app/assets/javascripts/custom-javascript.js
+++ app/assets/javascripts/custom-javascript.js
@@ -3,5 +3,37 @@ $(document).on('turbolinks:load', function() {
   if(myEle) {
     $('.footer').removeClass('bottom');
   }
+  $('#dropdownid').val('selectedvalue')
+  $("#change-leaderboard").on("click", function(){
+    let trailingValue = $("#selected-Trailing").val();
+    const url = new URL(window.location.href);
+    url.searchParams.set('trailing_value', trailingValue);
+    console.log(url.search)
+    // search_params.set('trailing_value', trailingValue);
+    // url.search = search_params.toString()
+    var new_url = url.toString();
+    console.log(new_url);
+    window.location=url;
 
+    //  changeLeaderboard();
+  })
+
+  // function changeLeaderboard(){
+
+  //   $.ajax({
+  //     url: "/trailing",
+  //     type: "post",
+  //     dataType: "html",
+  //     data: {
+  //       trailing: trailingValue,
+  //     },
+  //       action: 'update_trailing',
+  //       _method: 'put',
+  //     success: function(data) {
+  //       return
+  //     }, error: function (obj){
+  //       return
+  //     }
+  //   })
+  // }
 })
--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -2,6 +2,11 @@ class LeaderboardController < ApplicationController
   def index
     # @user_ids = Pushup.where(created_at: 90.days.ago..Time.now).group('user_id').order('sum_amount').sum('amount')
     # @leaders = User.where(id: @user_ids.keys)#.order('id')
+    if params.include?('trailing_value')
+      @trailing_val = params[:trailing_value]
+    else
+      @trailing_val = 90
+    end
     sql = "
       SELECT
           a.*,
@@ -15,12 +20,16 @@ class LeaderboardController < ApplicationController
           FROM
               pushups
           WHERE
-              EXTRACT(DAY FROM now()-created_at) <= 90
+              EXTRACT(DAY FROM now()-created_at) <= '#{@trailing_val}'
           GROUP BY user_id
       ) b ON a.id = b.user_id
       AND a.hide_profile = false
       ORDER BY total_pushup DESC
     "
     @leaders = ActiveRecord::Base.connection.execute(sql)
+  end
+
+  def update_trailing
+    redirect_to  action: :index, trailing_value: params[:trailing], status: :temporary_redirect
   end
 end
