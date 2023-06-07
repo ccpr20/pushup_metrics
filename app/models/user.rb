cc77f479fbf020627ee7141254453554031c4a77
Talha Meh <talhacodes@gmail.com>
2023-06-07 20:35:57 +0500

    fix user age validation issue.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates_presence_of :email
-  validate  :over_18
+  validate  :over_18, on: :create
   validates :terms_and_conditions, :acceptance => true
 
 	has_many :pushups, :dependent => :destroy
