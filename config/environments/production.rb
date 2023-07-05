0b0246a7aa79e9b5d0adc87930f839c7a3a5f1d0
Talha Mehmood <talhacodes@gmail.com>
2023-07-05 15:29:20 +0500

    force SSL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,6 +38,8 @@ Rails.application.configure do
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
+  config.force_ssl = true
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
